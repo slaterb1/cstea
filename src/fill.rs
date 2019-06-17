774be2957b42b41d93003934094ae1b22cde7d12
Benjamin Slater <ben@hackcapital.com>
2019-06-16 17:03:01 -0700

    started Generic Fill for csv

---
+++ src/fill.rs
@@ -0,0 +1,33 @@
+extern crate rettle;
+extern crate csv;
+
+use rettle::ingredient::{Ingredient, Argument};
+use rettle::brewer::{Brewery};
+use rettle::tea::Tea;
+
+use std::sync::{Arc, RwLock};
+use std::io::{BufReader};
+use std::fs::File;
+
+struct CsvArg {
+    filepath: String
+}
+
+pub fn fill_from_csv<T: Tea>(args: &Option<Box<dyn Argument + Send>>, brewery: &Brewery, recipe: Arc<RwLock<Vec<Box<dyn Ingredient + Send + Sync>>>>, TeaStruct: T) {
+   match args {
+       None => panic!("Need to pass \"filepath\" param!"),
+       Some(box_args) => {
+           let box_args = box_args.as_any().downcast_ref::<CsvArg>().unwrap();
+           let f = File::open(&box_args.filepath).unwrap();
+           let reader = BufReader::new(f);
+           let mut rdr = csv::Reader::from_reader(reader);
+           
+           //let mut tea_box: Vec<T> = vec![];
+           for result in rdr.records() {
+               let record = result.unwrap();
+               println!("{:?}", record);
+               //tea_box.push(record);
+           }
+       }
+   }
+}
