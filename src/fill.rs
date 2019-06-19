547e02aa0c58c9f6360ec467e96ebd6fa88d3cd9
Benjamin Slater <ben@hackcapital.com>
2019-06-18 22:05:25 -0700

    corrected spacing

--- src/fill.rs
+++ src/fill.rs
@@ -20,21 +20,21 @@ impl Argument for CsvArg {
     }
 }
 
-pub fn fill_from_csv<T: Tea + Send>(args: &Option<Box<dyn Argument + Send>>, brewery: &Brewery, recipe: Arc<RwLock<Vec<Box<dyn Ingredient + Send + Sync>>>>, TeaStruct: T) {
-   match args {
-       None => panic!("Need to pass \"filepath\" param!"),
-       Some(box_args) => {
-           let box_args = box_args.as_any().downcast_ref::<CsvArg>().unwrap();
-           let f = File::open(&box_args.filepath).unwrap();
-           let reader = BufReader::new(f);
-           let mut rdr = csv::Reader::from_reader(reader);
-           
-           //let mut tea_box: Vec<T> = vec![];
-           for result in rdr.records() {
-               let record = result.unwrap();
-               println!("{:?}", record);
-               //tea_box.push(record);
-           }
-       }
-   }
+pub fn fill_from_csv<T: Tea + Send + ?Sized>(args: &Option<Box<dyn Argument + Send>>, brewery: &Brewery, recipe: Arc<RwLock<Vec<Box<dyn Ingredient + Send + Sync>>>>, tea_struct: Box<T>) {
+    match args {
+        None => panic!("Need to pass \"filepath\" param!"),
+        Some(box_args) => {
+            let box_args = box_args.as_any().downcast_ref::<CsvArg>().unwrap();
+            let f = File::open(&box_args.filepath).unwrap();
+            let reader = BufReader::new(f);
+            let mut rdr = csv::Reader::from_reader(reader);
+            
+            //let mut tea_box: Vec<T> = vec![];
+            for result in rdr.records() {
+                let record = result.unwrap();
+                println!("{:?}", record);
+                //tea_box.push(record);
+            }
+        }
+    }
 }
