2fcf4175169224fcdcf7e5cfeb64b03e26e42cef
Benjamin Slater <ben@hackcapital.com>
2019-06-22 19:53:37 -0400

    added buffer length, further documentation

--- src/fill.rs
+++ src/fill.rs
@@ -17,7 +17,15 @@ use std::fmt::Debug;
 /// Data structure that holds the Ingredient params for FillCsTea.
 pub struct CsvArg {
     /// The filepath to the csv that will be processed.
-    pub filepath: String
+    filepath: String,
+    buffer_length: usize,
+}
+
+impl CsvArg {
+    pub fn new(filepath: &str, buffer_length: usize) -> CsvArg {
+        let filepath = String::from(filepath);
+        CsvArg { filepath, buffer_length }
+    }
 }
 
 impl Argument for CsvArg {
@@ -53,6 +61,19 @@ impl FillCsTea {
     }
 }
 
+/// Helper function that sends to batch request to Brewers for processing.
+///
+/// # Arguments
+///
+/// * `brewery` - Brewery that processes the data.
+/// * `recipe` - Recipe for the ETL used by the Brewery.
+/// * `tea_batch` - Current batch to be sent and processed
+fn call_brewery(brewery: &Brewery, recipe: Arc<RwLock<Vec<Box<dyn Ingredient + Send + Sync>>>>, tea_batch: Vec<Box<dyn Tea + Send>>) {
+    brewery.take_order(|| {
+        make_tea(tea_batch, recipe);
+    });
+}
+
 ///
 /// Implements the csv read, deserialization to specified data struct, and passes the data to the
 /// brewery for processing.
@@ -66,22 +87,30 @@ fn fill_from_csv<T: Tea + Send + Debug + ?Sized + 'static>(args: &Option<Box<dyn
     where for<'de> T: Deserialize<'de>
 {
     match args {
-        None => panic!("Need to pass \"filepath\" param!"),
+        None => panic!("Need to pass \"filepath\" and buffer_length params!"),
         Some(box_args) => {
+            // unwrap params
             let box_args = box_args.as_any().downcast_ref::<CsvArg>().unwrap();
+            
+            // initialize reader with specified file from path
             let f = File::open(&box_args.filepath).unwrap();
             let reader = BufReader::new(f);
             let mut rdr = csv::Reader::from_reader(reader);
             
-            let mut tea_batch: Vec<Box<dyn Tea + Send>> = vec![];
+            // iterate over csv lines and push data into processer
+            let mut tea_batch: Vec<Box<dyn Tea + Send>> = Vec::with_capacity(box_args.buffer_length);
             for result in rdr.deserialize() {
+                // check if batch size has been reached and send to brewers if so
+                if tea_batch.len() == box_args.buffer_length {
+                    let recipe = Arc::clone(&recipe);
+                    call_brewery(brewery, recipe, tea_batch);
+                    tea_batch = Vec::with_capacity(box_args.buffer_length);
+                }
                 let tea: T = result.unwrap();
                 tea_batch.push(Box::new(tea));
             }
             let recipe = Arc::clone(&recipe);
-            brewery.take_order(|| {
-                make_tea(tea_batch, recipe);
-            });
+            call_brewery(brewery, recipe, tea_batch);
         }
     }
 }
